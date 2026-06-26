775c8b362264e1588f4ad51532d84643fe0f40d4
MouraEnderson <mtxs2011@hotmail.com>
2026-06-25 22:30:17 -0300

    refactor: remove unused frontend config file

--- frontend/config.js
+++
@@ -1,6 +0,0 @@
-window.ASSESSMENT_CONFIG = Object.freeze({
-  apiBaseUrl: window.localStorage.getItem('assessment.apiBaseUrl') || '',
-  storageKey: 'assessment-report-builder.state.v1',
-  minimumTranscriptLength: 20,
-  officialWidgetPath: '/'
-});
